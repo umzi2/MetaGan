44dffae71cedd13b363197506cf7b48dcc77ea7e
umzi <ab5447612@gmail.com>
2024-11-13 11:53:08 +0300

    add Attention

--- MetaGan.py
+++ MetaGan.py
@@ -1,3 +1,4 @@
+import torch
 from einops import rearrange
 from torch import nn
 from typing import Sequence
@@ -43,6 +44,58 @@ class SepConv(nn.Module):
         return x
 
 
+class CrossAttention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        num_heads=8,
+        qkv_bias=False,
+        qk_scale=None,
+        attn_drop=0.0,
+        proj_drop=0.0,
+    ):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim**-0.5
+        self.wq = nn.Linear(dim, dim, bias=qkv_bias)
+        self.wk = nn.Linear(dim, dim, bias=qkv_bias)
+        self.wv = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = attn_drop
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x, _res):
+        B, N, C = x.shape
+        q = (
+            self.wq(x[:, 0:1, ...])
+            .reshape(B, 1, self.num_heads, C // self.num_heads)
+            .permute(0, 2, 1, 3)
+        )  # B1C -> B1H(C/H) -> BH1(C/H)
+        k = (
+            self.wk(x)
+            .reshape(B, N, self.num_heads, C // self.num_heads)
+            .permute(0, 2, 1, 3)
+        )  # BNC -> BNH(C/H) -> BHN(C/H)
+        v = (
+            self.wv(x)
+            .reshape(B, N, self.num_heads, C // self.num_heads)
+            .permute(0, 2, 1, 3)
+        )  # BNC -> BNH(C/H) -> BHN(C/H)
+        with torch.no_grad():
+            x = (
+                nn.functional.scaled_dot_product_attention(
+                    q, k, v, scale=self.scale, dropout_p=self.attn_drop
+                )
+                .transpose(1, 2)
+                .reshape(B, 1, C)
+            )
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
 class DWConv(nn.Module):
     def __init__(self, hidden_features):
         super(DWConv, self).__init__()
@@ -68,9 +121,13 @@ class DWConv(nn.Module):
 
 
 class Block(nn.Module):
-    def __init__(self, dim: int = 64):
+    def __init__(self, dim: int = 64, att=False, attn_drop=0.0, proj_drop=0.0):
         super().__init__()
-        self.t_mix = SepConv(dim)
+        self.t_mix = (
+            SepConv(dim)
+            if not att
+            else CrossAttention(dim, attn_drop=attn_drop, proj_drop=proj_drop)
+        )
         self.mlp = DWConv(dim)
         self.norm1 = nn.RMSNorm(dim)
         self.norm2 = nn.RMSNorm(dim)
@@ -81,9 +138,11 @@ class Block(nn.Module):
 
 
 class Blocks(nn.Module):
-    def __init__(self, dim, n_block):
+    def __init__(self, dim, n_block, att=False, attn_drop=0.0, proj_drop=0.0):
         super().__init__()
-        self.blocks = nn.ModuleList() + [Block(dim) for _ in range(n_block)]
+        self.blocks = nn.ModuleList() + [
+            Block(dim, att, attn_drop, proj_drop) for _ in range(n_block)
+        ]
 
     def forward(self, x):
         _B, _C, H, W = x.shape
@@ -111,15 +170,26 @@ class MetaGan(nn.Module):
         n_class: int = 1,
         dims: Sequence[int] = (48, 96, 192, 288),
         blocks: Sequence[int] = (3, 3, 9, 3),
+        attention: bool = True,
+        attn_drop: float = 0.0,
+        proj_drop: float = 0.0,
         drop: float = 0.2,
-        sigmoid: bool = False,
+        sigmoid: bool = True,
     ):
         super().__init__()
         dims = [in_ch] + list(dims)
         states = []
         for index in range(len(blocks)):
             states.append(Down(dims[index], dims[index + 1], 4 if index == 0 else 2))
-            states.append(Blocks(dims[index + 1], blocks[index]))
+            states.append(
+                Blocks(
+                    dims[index + 1],
+                    blocks[index],
+                    index > 1 if attention else False,
+                    attn_drop,
+                    proj_drop,
+                )
+            )
         self.blocks = nn.Sequential(*states)
         self.conv1 = nn.Sequential(
             nn.Conv2d(
